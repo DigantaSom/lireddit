e0387ecccb991a43ee8b5e909f5c7963e79cc034
Diganta Som <itsdsom111@gmail.com>
2021-03-16 20:53:49 +0530

    set up Redis

--- server/dist/index.js
+++ server/dist/index.js
@@ -14,9 +14,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const apollo_server_express_1 = require("apollo-server-express");
 const config_1 = __importDefault(require("config"));
+const connect_redis_1 = __importDefault(require("connect-redis"));
 const express_1 = __importDefault(require("express"));
+const express_session_1 = __importDefault(require("express-session"));
+const ioredis_1 = __importDefault(require("ioredis"));
 const type_graphql_1 = require("type-graphql");
 const typeorm_1 = require("typeorm");
+const constants_1 = require("./constants");
 const Post_1 = require("./entities/Post");
 const User_1 = require("./entities/User");
 const posts_1 = require("./resolvers/posts");
@@ -32,6 +36,24 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         entities: [User_1.User, Post_1.Post],
     });
     const app = express_1.default();
+    const redis = new ioredis_1.default();
+    const RedisStore = connect_redis_1.default(express_session_1.default);
+    app.use(express_session_1.default({
+        name: constants_1.COOKIE_NAME,
+        store: new RedisStore({
+            client: redis,
+            disableTouch: true,
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: constants_1.__prod__,
+        },
+        saveUninitialized: false,
+        secret: config_1.default.get('REDIS_SECRET'),
+        resave: false,
+    }));
     const apolloServer = new apollo_server_express_1.ApolloServer({
         schema: yield type_graphql_1.buildSchema({
             resolvers: [posts_1.PostResolver, user_1.UserResolver],
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import { ApolloServer } from 'apollo-server-express';
 import config from 'config';
+import connectRedis from 'connect-redis';
 import express from 'express';
+import session from 'express-session';
+import Redis from 'ioredis';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
+import { COOKIE_NAME, __prod__ } from './constants';
 import { Post } from './entities/Post';
 import { User } from './entities/User';
 import { PostResolver } from './resolvers/posts';
@@ -20,6 +24,28 @@ const main = async () => {
   });
 
   const app = express();
+
+  const redis = new Redis();
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: __prod__,
+      },
+      saveUninitialized: false,
+      secret: config.get('REDIS_SECRET'),
+      resave: false,
+    })
+  );
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
