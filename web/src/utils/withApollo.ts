ece66137b2c3fcfcc8cffb855a03a2ef51465fce
Diganta Som <itsdsom111@gmail.com>
2021-03-18 13:35:03 +0530

    SSR cookie forwarding

--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -1,32 +1,38 @@
-import { withApollo as createWithApollo } from 'next-apollo';
+import createWithApollo from './createWithApollo';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { NextPageContext } from 'next';
 import { PaginatedPosts } from '../generated/graphql';
+import { isServer } from './isServer';
 
-const createClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  credentials: 'include',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          posts: {
-            keyArgs: [],
-            merge(
-              existing: PaginatedPosts | undefined,
-              incoming: PaginatedPosts
-            ): PaginatedPosts {
-              // console.log(existing, incoming);
-              return {
-                __typename: 'PaginatedPosts',
-                hasMore: incoming.hasMore,
-                posts: [...(existing?.posts || []), ...incoming.posts],
-              };
+const createClient = (ctx: NextPageContext | undefined) =>
+  new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    credentials: 'include',
+    headers: {
+      cookie: isServer() ? ctx?.req?.headers.cookie || '' : '',
+    },
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            posts: {
+              keyArgs: [],
+              merge(
+                existing: PaginatedPosts | undefined,
+                incoming: PaginatedPosts
+              ): PaginatedPosts {
+                // console.log(existing, incoming);
+                return {
+                  __typename: 'PaginatedPosts',
+                  hasMore: incoming.hasMore,
+                  posts: [...(existing?.posts || []), ...incoming.posts],
+                };
+              },
             },
           },
         },
       },
-    },
-  }),
-});
+    }),
+  });
 
 export const withApollo = createWithApollo(createClient);
