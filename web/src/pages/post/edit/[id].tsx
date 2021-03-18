ebc0c02abfa43613c1f3840848c0dfb7896875fd
Diganta Som <itsdsom111@gmail.com>
2021-03-18 18:48:40 +0530

    Edit post functionality in client

--- web/src/pages/post/edit/[id].tsx
+++ web/src/pages/post/edit/[id].tsx
@@ -1,13 +1,61 @@
 import { Box, Button } from '@chakra-ui/react';
-import { Formik, Form } from 'formik';
+import { Form, Formik } from 'formik';
+import { useRouter } from 'next/router';
 import InputField from '../../../components/InputField';
 import Layout from '../../../components/Layout';
+import { usePostQuery, useUpdatePostMutation } from '../../../generated/graphql';
+import { useGetIntId } from '../../../utils/useGetIntId';
 import { withApollo } from '../../../utils/withApollo';
 
 const EditPost = () => {
+  const postId = useGetIntId();
+
+  const { data, loading, error } = usePostQuery({
+    variables: {
+      id: postId,
+    },
+    skip: postId === -1,
+  });
+  const [updatePost] = useUpdatePostMutation();
+  const router = useRouter();
+
+  if (loading) {
+    return (
+      <Layout>
+        <Box>Loading...</Box>
+      </Layout>
+    );
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>could not find post</Box>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return (
+      <Layout>
+        <Box>{error.message}</Box>
+      </Layout>
+    );
+  }
+
   return (
     <Layout variant='small'>
-      <Formik initialValues={{ title: '', text: '' }} onSubmit={async values => {}}>
+      <Formik
+        initialValues={{ title: data.post.title, text: data.post.text }}
+        onSubmit={async values => {
+          await updatePost({
+            variables: {
+              id: postId,
+              input: { ...values },
+            },
+          });
+          router.back();
+        }}>
         {({ isSubmitting }) => (
           <Form>
             <Box mt={4}>
@@ -17,7 +65,7 @@ const EditPost = () => {
               <InputField label='Edit Body' name='text' textarea />
             </Box>
             <Button type='submit' colorScheme='teal' mt={4} isLoading={isSubmitting}>
-              Post
+              Update Post
             </Button>
           </Form>
         )}
